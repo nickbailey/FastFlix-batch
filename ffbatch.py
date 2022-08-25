ec4a30edef66c066ff4174a7e254b333d4e5bd3c
Nick Bailey <nick@n-ism.org>
2022-08-25 13:20:31 +0100

    Add extra ssh options

--- ffbatch.py
+++ ffbatch.py
@@ -9,8 +9,9 @@ mp_dflt = '/auto/hamlet'
 # Path of the exported filesystem appearing at the above mount point
 export_dflt = '/export'
 
-# Default local ssh command to run commands on the remote machine
-ssh_dflt = f'/usr/bin/ssh {os.getlogin()}@hamlet'
+# Default local ssh command etc to run commands on the remote machine
+ssh_dflt = '/usr/bin/ssh'
+sshhost_dflt = 'hamlet'
 
 # Use clp to parse the command line
 clp = argparse.ArgumentParser(
@@ -25,17 +26,22 @@ clp.add_argument('--list', action='store_true',
                  help='''List all jobs read before starting''')
 clp.add_argument('--sshcmd', type=str, default=ssh_dflt,
                  help='''ssh command to run exectuables on the remote machine''')
+clp.add_argument('--sshuser', type=str, default=os.getlogin(),
+		 help='''Login name of account on remote machine''')
+clp.add_argument('--sshhost', type=str, default=sshhost_dflt,
+		 help='''Remote host which will be running the encoding jobs''')
 clp.add_argument('queue', type=str,
                  help='''YAML file containing the exported FastFlix job list''')
 
 args = clp.parse_args()
 
 # Run a remote command on the server
-def exec_remote(cmd):
-    res, err = subprocess.Popen(f'{args.sshcmd} {cmd}',
-                                shell=True,
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-    return res.decode('utf-8'), err.decode('utf8')
+def exec_remote(cmd : str) -> (str, str, int) :
+    sp = subprocess.Popen(f'{args.sshcmd} {args.sshuser}@{args.sshhost} {cmd}',
+                          shell=True,
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res, err = sp.communicate()
+    return (res.decode('utf-8'), err.decode('utf8'), sp.returncode)
 
 # Read the whole of the named job queue from the yaml file.
 with open(args.queue, "r") as stream:
@@ -60,10 +66,13 @@ for j in q['queue'] :
 
 # If there are any jobs to do, create a temp directory on the host to upload the cover art.
 if j :
-    remote_tempdir = exec_remote('mktemp FastFlix-covers.XXXXXX')
-    cover_num = 0
+    r_tmpdir, r_errs, r_rc = exec_remote('mktemp --tmpdir FastFlix-covers.XXXXXX')
+
+print(r_tmpdir, r_errs, r_rc)
 
 # Upload cover art
+cover_num = 0
+
 
 # Convert the encoding commands' paths to run on the server
 
