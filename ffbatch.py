db939c481c3aa39ce366304ac876643588a35ff9
Nick Bailey <nick@n-ism.org>
2022-08-26 15:56:47 +0100

    Fix up ffbatch to use async ssh

--- ffbatch.py
+++ ffbatch.py
@@ -3,6 +3,7 @@ import yaml
 import argparse
 import subprocess
 import asyncio, asyncssh
+from sshSession import sshSession
 
 # Path where exported filesystems are mounted on the client
 mp_dflt = '/auto/hamlet'
@@ -11,7 +12,6 @@ mp_dflt = '/auto/hamlet'
 export_dflt = '/export'
 
 # Default local ssh command etc to run commands on the remote machine
-ssh_dflt = '/usr/bin/ssh'
 sshhost_dflt = 'hamlet'
 
 # Use clp to parse the command line
@@ -25,9 +25,7 @@ clp.add_argument('--export', type=str, default=export_dflt,
                  help='''Path exported by the media server to the client''')
 clp.add_argument('--list', action='store_true',
                  help='''List all jobs read before starting''')
-clp.add_argument('--sshcmd', type=str, default=ssh_dflt,
-                 help='''ssh command to run exectuables on the remote machine''')
-clp.add_argument('--sshuser', type=str, default=os.getlogin(),
+clp.add_argument('--sshuser', type=str,
 		 help='''Login name of account on remote machine''')
 clp.add_argument('--sshhost', type=str, default=sshhost_dflt,
 		 help='''Remote host which will be running the encoding jobs''')
@@ -36,19 +34,7 @@ clp.add_argument('queue', type=str,
 
 args = clp.parse_args()
 
-async def ssh_session() -> None :
-    async with asyncssh.connect(sshhost, username=sshuser) as conn:
-        result = await conn.run('ls abc', check=True)
-        yield (result.stdout, end='')
-
 async def main() -> None :
-    # Run a remote command on the server
-    def exec_remote(cmd : str) -> (str, str, int) :
-        sp = subprocess.Popen(f'{args.sshcmd} {args.sshuser}@{args.sshhost} {cmd}',
-                            shell=True,
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        res, err = sp.communicate()
-        return (res.decode('utf-8'), err.decode('utf8'), sp.returncode)
 
     # Read the whole of the named job queue from the yaml file.
     with open(args.queue, "r") as stream:
@@ -71,22 +57,29 @@ async def main() -> None :
             desc['cover'] = None
         jobs.append(desc)
 
+    if args.list > 0:
+        for j in jobs :
+            print (f"\n{j['video_title']}\n==========================")
+            print (f"{j['encode_command']}\n--------------------------")
+            print (f"Cover: {j['cover']}\n==========================")
+
+    # Start a remote session on the server
+    sshargs = {}
+    if args.sshuser :
+        sshargs['username'] = args.sshuser
+    print(sshargs)
+    rs = sshSession(await asyncssh.connect(args.sshhost, **sshargs))
+
     # If there are any jobs to do, create a temp directory on the host to upload the cover art.
     if j :
-        r_tmpdir, r_errs, r_rc = exec_remote('mktemp --tmpdir FastFlix-covers.XXXXXX')
+        r_tmpdir, r_errs, r_rc = await rs.cmd('mktemp --tmpdir FastFlix-covers.XXXXXX')
 
     print(r_tmpdir, r_errs, r_rc)
 
     # Upload cover art
     cover_num = 0
 
-
     # Convert the encoding commands' paths to run on the server
 
-    for j in jobs :
-        if args.list:
-            print (f"\n{j['video_title']}\n==========================")
-            print (f"{j['encode_command']}\n--------------------------")
-            print (f"Cover: {j['cover']}\n==========================")
 
 asyncio.run(main())
